a09dd4b8d597253ec29363ccabbe6bb45fe9aa41
süleyman sekman <ssekman42@gmail.com>
2025-05-21 13:44:55 +0300

    Update IsEmrineBagliUretimSorgu.dart

--- lib/data/datasources/is_emrine_bagliuretim_sorgulamalar/IsEmrineBagliUretimSorgu.dart
+++ lib/data/datasources/is_emrine_bagliuretim_sorgulamalar/IsEmrineBagliUretimSorgu.dart
@@ -8,8 +8,8 @@ class IsEmrineBagliUretimGirisSorgu {
     connectTimeout: Duration(seconds: 10),
     receiveTimeout: Duration(seconds: 10),
   ));
-  final String endpoint = 'http://195.175.82.182:8080/CaniasWS-v1/services/iasWebService';
-  final String loginUrl = 'http://195.175.82.182/terminal/login.aspx/login.aspx';
+  final String endpoint = '';
+  final String loginUrl = '';
   String? _sessionId;
   final CookieJar cookieJar = CookieJar();
 
@@ -25,17 +25,17 @@ class IsEmrineBagliUretimGirisSorgu {
   Future<String> _getSessionId() async {
     final String envelope = '''
 <?xml version="1.0" encoding="utf-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="" xmlns:web="">
   <soapenv:Header/>
   <soapenv:Body>
     <web:login>
-      <p_strClient>00</p_strClient>
-      <p_strLanguage>T</p_strLanguage>
-      <p_strDBName>BIEN802</p_strDBName>
-      <p_strDBServer>CANIAS</p_strDBServer>
-      <p_strAppServer>195.175.82.182:27499</p_strAppServer>
-      <p_strUserName>BIENURETIM</p_strUserName>
-      <p_strPassword>kp2010</p_strPassword>
+      <p_strClient></p_strClient>
+      <p_strLanguage></p_strLanguage>
+      <p_strDBName></p_strDBName>
+      <p_strDBServer></p_strDBServer>
+      <p_strAppServer></p_strAppServer>
+      <p_strUserName></p_strUserName>
+      <p_strPassword></p_strPassword>
     </web:login>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -52,8 +52,8 @@ class IsEmrineBagliUretimGirisSorgu {
             'SOAPAction': '',
             'User-Agent': 'Mozilla/5.0 (compatible; BienTerminalWeb)',
             'Accept': 'text/xml, application/xml',
-            'Host': '195.175.82.182:8080',
-            'Connection': 'keep-alive',
+            'Host': '',
+            'Connection': '',
           },
         ),
         data: envelope,
@@ -96,11 +96,11 @@ class IsEmrineBagliUretimGirisSorgu {
   <soapenv:Header/>
   <soapenv:Body>
     <web:callIASService>
-      <sessionid>$_sessionId</sessionid>
-      <serviceid>ISEMIRURETIMGIRIS</serviceid>
-      <args>$parametre</args>
-      <returntype>STRING</returntype>
-      <permanent>true</permanent>
+      <sessionid></sessionid>
+      <serviceid></serviceid>
+      <args>$</args>
+      <returntype></returntype>
+      <permanent></permanent>
     </web:callIASService>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -123,7 +123,7 @@ class IsEmrineBagliUretimGirisSorgu {
             'SOAPAction': '',
             'User-Agent': 'Mozilla/5.0 (compatible; BienTerminalWeb)',
             'Accept': 'text/xml, application/xml',
-            'Host': '195.175.82.182:8080',
+            'Host': '',
             'Connection': 'keep-alive',
           },
           responseType: ResponseType.plain, // Metin yanıtı al
