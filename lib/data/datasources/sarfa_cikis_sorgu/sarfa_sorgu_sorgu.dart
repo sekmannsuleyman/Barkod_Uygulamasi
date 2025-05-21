cc9b95ca411dd6096bde2f23ae35becd16432b90
süleyman sekman <ssekman42@gmail.com>
2025-05-21 13:52:34 +0300

    Update sarfa_sorgu_sorgu.dart

--- lib/data/datasources/sarfa_cikis_sorgu/sarfa_sorgu_sorgu.dart
+++ lib/data/datasources/sarfa_cikis_sorgu/sarfa_sorgu_sorgu.dart
@@ -6,7 +6,7 @@ class SarfaSorguSorgu {
     connectTimeout: Duration(seconds: 10),
     receiveTimeout: Duration(seconds: 10),
   ));
-  final String endpoint = 'http://195.175.82.182:8080/CaniasWS-v1/services/iasWebService';
+  final String endpoint = 'he';
   String? _sessionId;
 
   // Sarfa Çıkış Sorgu
@@ -28,15 +28,15 @@ class SarfaSorguSorgu {
 
     final envelope = '''
 <?xml version="1.0" encoding="utf-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="" xmlns:web="">
   <soapenv:Header/>
   <soapenv:Body>
     <web:callIASService>
-      <sessionid>$_sessionId</sessionid>
+      <sessionid>$</sessionid>
       <serviceid>SARF</serviceid>
-      <args>$parametre</args>
-      <returntype>STRING</returntype>
-      <permanent>true</permanent>
+      <args>$</args>
+      <returntype></returntype>
+      <permanent></permanent>
     </web:callIASService>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -97,17 +97,17 @@ class SarfaSorguSorgu {
   Future<String> _getSessionId() async {
     final envelope = '''
 <?xml version="1.0" encoding="utf-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="ht/" xmlns:web="ht">
   <soapenv:Header/>
   <soapenv:Body>
     <web:login>
-      <p_strClient>00</p_strClient>
-      <p_strLanguage>T</p_strLanguage>
-      <p_strDBName>BIEN802</p_strDBName>
-      <p_strDBServer>CANIAS</p_strDBServer>
-      <p_strAppServer>195.175.82.182:27499</p_strAppServer>
-      <p_strUserName>BIENURETIM</p_strUserName>
-      <p_strPassword>kp2010</p_strPassword>
+      <p_strClient></p_strClient>
+      <p_strLanguage></p_strLanguage>
+      <p_strDBName></p_strDBName>
+      <p_strDBServer></p_strDBServer>
+      <p_strAppServer></p_strAppServer>
+      <p_strUserName></p_strUserName>
+      <p_strPassword></p_strPassword>
     </web:login>
   </soapenv:Body>
 </soapenv:Envelope>
