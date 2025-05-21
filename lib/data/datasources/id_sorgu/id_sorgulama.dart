3d36adb6e1f08dad965edceb43a6a848e57a2c1b
süleyman sekman <ssekman42@gmail.com>
2025-05-21 13:42:27 +0300

    Update id_sorgulama.dart

--- lib/data/datasources/id_sorgu/id_sorgulama.dart
+++ lib/data/datasources/id_sorgu/id_sorgulama.dart
@@ -3,7 +3,7 @@ import 'package:xml/xml.dart' as xml;
 
 class IdSorgulama {
   final Dio dio = Dio();
-  final String endpoint = 'http://195.175.82.182:8080/CaniasWS-v1/services/iasWebService';
+  final String endpoint = '';
   String? _sessionId;
 
   Future<void> _ensureSession() async {
@@ -12,17 +12,17 @@ class IdSorgulama {
 
   Future<String> _getSessionId() async {
     final String envelope = '''
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="" xmlns:web="">
   <soapenv:Header/>
   <soapenv:Body>
     <web:login>
-      <p_strClient>00</p_strClient>
-      <p_strLanguage>T</p_strLanguage>
-      <p_strDBName>BIEN802</p_strDBName>
-      <p_strDBServer>CANIAS</p_strDBServer>
-      <p_strAppServer>195.175.82.182:27499</p_strAppServer>
-      <p_strUserName>BIENURETIM</p_strUserName>
-      <p_strPassword>kp2010</p_strPassword>
+      <p_strClient></p_strClient>
+      <p_strLanguage></p_strLanguage>
+      <p_strDBName></p_strDBName>
+      <p_strDBServer></p_strDBServer>
+      <p_strAppServer></p_strAppServer>
+      <p_strUserName></p_strUserName>
+      <p_strPassword></p_strPassword>
     </web:login>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -64,15 +64,15 @@ class IdSorgulama {
     await _ensureSession();
 
     final String envelope = '''
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="" xmlns:web="">
   <soapenv:Header/>
   <soapenv:Body>
     <web:callIASService>
-      <sessionid>$_sessionId</sessionid>
-      <serviceid>IDSORGULA</serviceid>
+      <sessionid>$</sessionid>
+      <serviceid></serviceid>
       <args>$id</args>
-      <returntype>STRING</returntype>
-      <permanent>true</permanent>
+      <returntype></returntype>
+      <permanent></permanent>
     </web:callIASService>
   </soapenv:Body>
 </soapenv:Envelope>
