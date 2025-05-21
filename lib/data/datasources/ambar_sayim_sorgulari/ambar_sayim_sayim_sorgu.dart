909bf0f4504f405ebaa96ac9653a78efdd47a73a
süleyman sekman <ssekman42@gmail.com>
2025-05-21 13:35:44 +0300

    Update ambar_sayim_sayim_sorgu.dart

--- lib/data/datasources/ambar_sayim_sorgulari/ambar_sayim_sayim_sorgu.dart
+++ lib/data/datasources/ambar_sayim_sorgulari/ambar_sayim_sayim_sorgu.dart
@@ -14,15 +14,15 @@ class AmbarSayimSorguSorgu {
 
     final parametre = "$rafNo,$gozNo,$barkodNo";
     final String envelope = '''
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="" xmlns:web="">
   <soapenv:Header/>
   <soapenv:Body>
     <web:callIASService>
-      <sessionid>$_sessionId</sessionid>
-      <serviceid>AMBARSAYIMSORGU</serviceid>
-      <args>$parametre</args>
-      <returntype>STRING</returntype>
-      <permanent>true</permanent>
+      <sessionid></sessionid>
+      <serviceid></serviceid>
+      <args></args>
+      <returntype></returntype>
+      <permanent></permanent>
     </web:callIASService>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -33,7 +33,7 @@ class AmbarSayimSorguSorgu {
         endpoint,
         options: Options(headers: {
           'Content-Type': 'text/xml; charset=utf-8',
-          'SOAPAction': 'http://web.ias.com/callIASService',
+          'SOAPAction': '',
         }),
         data: envelope,
       );
@@ -88,13 +88,13 @@ class AmbarSayimSorguSorgu {
   <soapenv:Header/>
   <soapenv:Body>
     <web:login>
-      <p_strClient>00</p_strClient>
-      <p_strLanguage>T</p_strLanguage>
-      <p_strDBName>BIEN802</p_strDBName>
-      <p_strDBServer>CANIAS</p_strDBServer>
-      <p_strAppServer>195.175.82.182:27499</p_strAppServer>
-      <p_strUserName>BIENURETIM</p_strUserName>
-      <p_strPassword>kp2010</p_strPassword>
+      <p_strClient></p_strClient>
+      <p_strLanguage></p_strLanguage>
+      <p_strDBName></p_strDBName>
+      <p_strDBServer></p_strDBServer>
+      <p_strAppServer></p_strAppServer>
+      <p_strUserName></p_strUserName>
+      <p_strPassword></p_strPassword>
     </web:login>
   </soapenv:Body>
 </soapenv:Envelope>
