68456d80b451d3cc4380c8f15286f2e4571b4107
süleyman sekman <ssekman42@gmail.com>
2025-05-21 14:07:19 +0300

    Update urun_id_sorgulama.dart

--- lib/data/datasources/urun_id_sorgu/urun_id_sorgulama.dart
+++ lib/data/datasources/urun_id_sorgu/urun_id_sorgulama.dart
@@ -4,7 +4,7 @@ import 'package:xml/xml.dart' as xml;
 class UrunIdSorgulama {
   final Dio dio = Dio();
   final String endpoint =
-      'http://195.175.82.182:8080/CaniasWS-v1/services/iasWebService';
+      '';
 
   String? _sessionId; // login sadece 1 kez yapılacak
 
@@ -27,15 +27,15 @@ class UrunIdSorgulama {
     final String parameter = '$urunId,${benzerParti ? '1' : '0'}';
 
     final String soapEnvelope = '''
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="h" xmlns:web="h">
   <soapenv:Header/>
   <soapenv:Body>
     <web:callIASService>
-      <sessionid>$_sessionId</sessionid>
-      <serviceid>$serviceName</serviceid>
-      <args>$parameter</args>
-      <returntype>STRING</returntype>
-      <permanent>true</permanent>
+      <sessionid>$</sessionid>
+      <serviceid>$</serviceid>
+      <args>$</args>
+      <returntype></returntype>
+      <permanent></permanent>
     </web:callIASService>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -108,17 +108,17 @@ class UrunIdSorgulama {
   // Giriş (login)
   Future<String> _getSessionId() async {
     final String loginEnvelope = '''
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="http" xmlns:web="hm">
   <soapenv:Header/>
   <soapenv:Body>
     <web:login>
-      <p_strClient>00</p_strClient>
-      <p_strLanguage>T</p_strLanguage>
-      <p_strDBName>BIEN802</p_strDBName>
-      <p_strDBServer>CANIAS</p_strDBServer>
-      <p_strAppServer>195.175.82.182:27499</p_strAppServer>
-      <p_strUserName>BIENURETIM</p_strUserName>
-      <p_strPassword>kp2010</p_strPassword>
+      <p_strClient></p_strClient>
+      <p_strLanguage></p_strLanguage>
+      <p_strDBName></_strDBName>
+      <p_strDBServer></p_strDBServer>
+      <p_strAppServer>19</p_strAppServer>
+      <p_strUserName></p_strUserName>
+      <p_strPassword></p_strPassword>
     </web:login>
   </soapenv:Body>
 </soapenv:Envelope>
