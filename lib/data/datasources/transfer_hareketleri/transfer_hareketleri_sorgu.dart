513b0872aebc7a58ac0b1e38cd789a2eebc9d19f
süleyman sekman <ssekman42@gmail.com>
2025-05-21 14:02:11 +0300

    Update transfer_hareketleri_sorgu.dart

--- lib/data/datasources/transfer_hareketleri/transfer_hareketleri_sorgu.dart
+++ lib/data/datasources/transfer_hareketleri/transfer_hareketleri_sorgu.dart
@@ -9,7 +9,7 @@ class TransferHareketleriSorgu {
     ),
   );
   final String endpoint =
-      'http://195.175.82.182:8080/CaniasWS-v1/services/iasWebService';
+      'he';
   String? _sessionId;
 
   Future<void> _ensureSession() async {
@@ -20,17 +20,17 @@ class TransferHareketleriSorgu {
     final String envelope =
         '''
 <?xml version="1.0" encoding="utf-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="ht/" xmlns:web="htm">
   <soapenv:Header/>
   <soapenv:Body>
     <web:login>
-      <p_strClient>00</p_strClient>
-      <p_strLanguage>T</p_strLanguage>
-      <p_strDBName>BIEN802</p_strDBName>
-      <p_strDBServer>CANIAS</p_strDBServer>
-      <p_strAppServer>195.175.82.182:27499</p_strAppServer>
-      <p_strUserName>BIENURETIM</p_strUserName>
-      <p_strPassword>kp2010</p_strPassword>
+      <p_strClient></p_strClient>
+      <p_strLanguage></p_strLanguage>
+      <p_strDBName></p_strDBName>
+      <p_strDBServer></p_strDBServer>
+      <p_strAppServer>1</p_strAppServer>
+      <p_strUserName></p_strUserName>
+      <p_strPassword></p_strPassword>
     </web:login>
   </soapenv:Body>
 </soapenv:Envelope>
@@ -97,15 +97,15 @@ class TransferHareketleriSorgu {
     final String envelope =
         '''
 <?xml version="1.0" encoding="utf-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://web.ias.com">
+<soapenv:Envelope xmlns:soapenv="http:///" xmlns:web="ht">
   <soapenv:Header/>
   <soapenv:Body>
     <web:callIASService>
-      <sessionid>$_sessionId</sessionid>
-      <serviceid>$serviceId</serviceid>
-      <args>$parametre</args>
-      <returntype>STRING</returntype>
-      <permanent>true</permanent>
+      <sessionid>$</sessionid>
+      <serviceid>$</serviceid>
+      <args>$</args>
+      <returntype></returntype>
+      <permanent></permanent>
     </web:callIASService>
   </soapenv:Body>
 </soapenv:Envelope>
